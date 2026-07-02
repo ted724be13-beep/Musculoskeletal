5bdfeb7ac6c9a4d2658e7fc8817e57d6f23caca8
ted724be13-beep <ted724.be13@nycu.edu.tw>
2026-07-02 16:52:08 +0800

    Remove comment from vite.config.js
    Removed comment explaining the purpose of environment variable handling.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-    // 這段代碼負責把 Vercel 後台的環境變數傳遞給前端代碼
     __firebase_config: JSON.stringify(process.env.__firebase_config || '{}'),
     __app_id: JSON.stringify(process.env.__app_id || '"default-app"')
   }
